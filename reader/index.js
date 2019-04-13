f78dceb1903587b2f4184f4539cb243e4b76b8fa
Mario Mol <mariohmol@gmail.com>
2019-04-12 23:56:47 -0300

    getting from a list of terms

--- reader/index.js
+++ reader/index.js
@@ -2,36 +2,39 @@
 
 const { readerDownload } = require('./download.js');
 const { convertToText } = require('./convert.js');
-const { readFile, readFolder } = require('./read.js');
+const { readFile, readFolder, readTerms } = require('./read.js');
 
 
-function getDay(day){
+function getDay(day) {
   readerDownload(day);
   convertToText(day);
 }
 const terms = [
-    '1111111-62.2016.8.26.0100',
-    '0352286-78.2019.8.13.0000',
-    '6024116-34.2015.8.13.0024'
-  ]
+  { term: '1111111-62.2016.8.26.0100', tj: 'sp' },
+  { term: '0352286-78.2019.8.13.0000', tj: 'mg' },
+  { term: '6024116-34.2015.8.13.0024', tj: 'mg' }
+];
+
 const day = '2019-04-08';
 const state = 'mg';
-const folder = `download/${day}/tj${state}_dje_${day}_txt/`
+
 // readerDownload(day, 'TJMG');
+
+readTerms(day, terms);
 
 // getDay(day);
 // convertToText(day);
-readFolder(folder,terms);
+// const folder = `download/${day}/tj${state}_dje_${day}_txt/`
 // readFile('download/2019-04-08/tjsp_dje_2019-04-08_txt/tjsp_11_2019-04-08.txt',terms);
 
 /**
  * EXEMPLOS
- * 
+ *
  * readerDownload('2019-04-08');
  * readerDownload('2019-04-07', 'TJSP');
- * 
+ *
  * convertToText('2019-04-08');
- * 
+ *
  * readFile('download/2019-04-08/tjsp_dje_2019-04-08_txt/tjsp_11_2019-04-08.txt',[
   '1111111-62.2016.8.26.0100',
   '-62.2016.8.26.0100'
--- reader/read.js
+++ reader/read.js
@@ -3,12 +3,12 @@ const es = require('event-stream');
 const async = require('async');
 const path = require('path');
 
-const readFile = function (file, terms, cb=null) {
+const readFile = function (file, terms, found = {}, cb = null) {
   lineNr = 0;
-  let currentProcess;
-  let found = [];
   let linein = false;
   let currentLine;
+  let currentTerm;
+
   var s = fs.createReadStream(file)
     .pipe(es.split())
     .pipe(es.mapSync(function (line) {
@@ -18,19 +18,20 @@ const readFile = function (file, terms, cb=null) {
       // console.log(line , '-------------')
       lineNr += 1;
 
-      if(linein){
-        if(line.length < 5){
+      if (linein) {
+        if (line.length < 5) {
           linein = false
-          found.push(currentLine)
-        }else{
-          currentLine = currentLine+' '+line;
+          found[currentTerm] = currentLine;
+        } else {
+          currentLine = currentLine + ' ' + line;
         }
       }
 
       terms.forEach(term => {
         if (line.indexOf(term) >= 0) {
           currentLine = line;
-          linein=true;
+          currentTerm = term;
+          linein = true;
         }
       });
 
@@ -42,13 +43,13 @@ const readFile = function (file, terms, cb=null) {
       s.resume();
     })
       .on('error', function (err) {
-        if(cb){
+        if (cb) {
           cb()
         }
         console.log('Error while reading file.', err);
       })
       .on('end', function () {
-        if(cb){
+        if (cb) {
           console.log(found)
           cb(found)
         }
@@ -58,24 +59,29 @@ const readFile = function (file, terms, cb=null) {
 
 }
 
-const readFolder = function (folder ,terms) {
-  
-  // Read all files in ./incrementals folder
-  fs.readdir(folder, (e, files) => {
-    if(!files){
-      return;
-    }
-    async.filterSeries(files.sort(), (file, cb) => {
-      console.log('File: ', folder+'/'+file);
-      if(file.indexOf('.zip')>0 || file.indexOf('.html')>0){
-        cb();
-      }else{
-        readFile(folder+'/' + file, terms, cb);
+const readFolder = function (folder, terms) {
+
+  return new Promise((resolve, reject) => {
+    let found = {};
+    // Read all files in ./incrementals folder
+    fs.readdir(folder, (e, files) => {
+      if (!files) {
+        return;
       }
-    }, (err, validFiles) => {
-      console.log(validFiles);  // => Prints a list of all valid files
+      async.filterSeries(files.sort(), (file, cb) => {
+        console.log('File: ', folder + '/' + file);
+        if (file.indexOf('.zip') > 0 || file.indexOf('.html') > 0) {
+          cb();
+        } else {
+          readFile(folder + '/' + file, terms, found, cb);
+        }
+      }, (err, validFiles) => {
+        console.log(validFiles);  // => Prints a list of all valid files
+      });
+      resolve(found);
     });
-  });
+  })
+
 }
 
 function readAllLines(folder) {
@@ -93,4 +99,23 @@ function readAllLines(folder) {
     cb(null, true);
   });
 }
-module.exports = { readFile, readFolder, readAllLines };
+
+const readTerms = function (day, terms) {
+  const termsByTJ = {};
+  terms.forEach(t => {
+    if (termsByTJ[t.tj]) {
+      termsByTJ[t.tj].push(t.term);
+    } else {
+      termsByTJ[t.tj] = [t.term];
+    }
+  });
+
+  for (key in termsByTJ) {
+    const folderTJ = `download/${day}/tj${key}_dje_${day}_txt/`
+    readFolder(folderTJ, termsByTJ[key])
+      .then(found => {
+        console.log(found);
+      })
+  }
+}
+module.exports = { readFile, readFolder, readAllLines, readTerms };
